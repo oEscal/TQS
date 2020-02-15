1240333f3d01a01d8c66f6a1c0869d0402a2e9ca
escaleira <pedroescaleira@hotmail.com>
2020-02-15 22:52:01 +0000

    simple fix

--- aula1/ex1/src/test/java/tqsua/lab1stack/ITqsStackTest.java
+++ aula1/ex1/src/test/java/tqsua/lab1stack/ITqsStackTest.java
@@ -1,12 +1,12 @@
+package tqsua.lab1stack;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.function.Executable;
 import tqsua.lab1stack.ITqsStack;
 
 import java.util.NoSuchElementException;
@@ -14,11 +14,6 @@ import java.util.NoSuchElementException;
 public class ITqsStackTest {
 
     private ITqsStack<Integer> test_stack;
-
-    public ITqsStackTest() {
-        test_stack = new ITqsStack<Integer>();
-    }
-
 
     @BeforeAll
     static void setup(){
@@ -28,10 +23,9 @@ public class ITqsStackTest {
     @BeforeEach
     void setupThis(){
         System.out.println("@BeforeEach executed");
+        this.test_stack = new ITqsStack<Integer>();
     }
 
-    // @Tag("DEV")
-    // TODO -> ver o que significam estas tags
     @Test
     void testInitEmpty()
     {
@@ -39,7 +33,6 @@ public class ITqsStackTest {
         Assertions.assertEquals( true, this.test_stack.isEmpty());
     }
 
-    @Tag("PROD")
     @Test
     void testInitSize()
     {
@@ -55,11 +48,10 @@ public class ITqsStackTest {
         int expected_final_size = 100;
         this.pushN(expected_final_size);
 
-        Assertions.assertEquals( false, this.test_stack.isEmpty());
+        Assertions.assertFalse(this.test_stack.isEmpty());
         Assertions.assertEquals( expected_final_size, this.test_stack.size());
     }
 
-    @Tag("PROD")
     @Test
     void testPushThenPop()
     {
@@ -84,20 +76,17 @@ public class ITqsStackTest {
         System.out.println("======TEST SIX EXECUTED======");
 
         int n = 100;
-        this.test_stack.clear();
 
         this.pushN(n);
         this.popN(n);
 
-        Assertions.assertEquals(true, this.test_stack.isEmpty());
+        Assertions.assertTrue(this.test_stack.isEmpty());
         Assertions.assertEquals(0, this.test_stack.size());
     }
 
     @Test
     void testPopEmpty() {
         System.out.println("======TEST SIX EXECUTED======");
-
-        this.test_stack.clear();
         Assertions.assertThrows(NoSuchElementException.class, () -> ITqsStackTest.this.test_stack.pop());
     }
 
@@ -105,7 +94,6 @@ public class ITqsStackTest {
     void testPeekEmpty() {
         System.out.println("======TEST SEVEN EXECUTED======");
 
-        this.test_stack.clear();
         Assertions.assertThrows(NoSuchElementException.class, () -> ITqsStackTest.this.test_stack.peek());
     }
 
@@ -114,7 +102,6 @@ public class ITqsStackTest {
         System.out.println("======TEST EIGHT EXECUTED======");
 
         int max_size = 100;
-        this.test_stack.clear();
         this.test_stack.defineMaxSize(max_size);
 
         for (int i = 0; i < max_size; i++)
