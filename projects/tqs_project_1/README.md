5edc98c0d258e894d6e9c5437d2b5da2dd55c038
escaleira <pedroescaleira@hotmail.com>
2020-04-03 17:44:57 +0100

    modified gitignore

--- projects/tqs_project_1/README.md
+++
@@ -1,2 +0,0 @@
-# tqs_project_1
-Multi-layer web application, in Spring Boot, supplied with automated tests (at multiple levels)
--- projects/tqs_project_1/api/src/main/java/com/tqs1/api/service/HttpClient.java
+++
@@ -1,8 +0,0 @@
-package com.tqs1.api.service;
-
-import java.io.IOException;
-
-public interface HttpClient {
-
-    String get(String s) throws IOException;
-}
